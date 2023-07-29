98b563a899512ffa8388a78775dc5f78f16cbe85
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2023-07-29 10:19:52 +0200

    added borr technologies mother company

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -214,7 +214,8 @@ export default function RootLayout({
                             ))}
                         </div>
                         <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
-                            &copy; 2023 Verifiable Draws - All rights reserved.
+                            Verifiable Draws is the property of <Link href="https://www.borr.tech/" target="_blank" className="underline hover:text-gray-500">Borr Technologies</Link>, a registered French company.<br />
+                            &copy; 2023 Borr Technologies - All rights reserved.
                         </p>
                     </div>
                 </div>
