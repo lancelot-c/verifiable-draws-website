51b358c776af89d8c4f350efca914f1b1a7cf921
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2023-08-04 10:26:59 +0200

    test bis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -180,7 +180,7 @@ export default function Example() {
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
                         <div className="mx-auto max-w-2xl text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                Si cela fonctionne c'est sympa !
+                                Si cela fonctionne cest sympa !
                             </h1>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
                                 image.
