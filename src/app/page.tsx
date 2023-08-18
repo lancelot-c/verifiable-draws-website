631ab5beeb99a43d241f92b412cb653e5d560218
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2023-08-18 12:39:55 +0200

    wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -316,10 +316,10 @@ export default function Example() {
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
                         <div className="mx-auto max-w-3xl text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                The ultimate random draw platform
+                                Verifiable Randomness<br />for everyone
                             </h1>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
-                                <span className="italic">Verifiable Draws</span> is the first decentralized random draw platform in the world, running on the Ethereum blockchain with a verifiable source of randomness, unlocking a level of security and transparency that no existing solution can provide.
+                                <span className="italic">Verifiable Draws</span> is the first decentralized random draw platform in the world, providing a cryptographically secure and verifiable source of randomness for your random-based applications.
                             </p>
                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                 <Link
@@ -553,7 +553,7 @@ export default function Example() {
                         </dl>
                         <div className="text-center">
                             <p className="mt-16 text-lg leading-8 text-gray-600">
-                                Stay safe, protect your draws with Verifiable Draws.
+                                Stay safe, secure your draws with Verifiable Draws.
                             </p>
                         </div>
                         
