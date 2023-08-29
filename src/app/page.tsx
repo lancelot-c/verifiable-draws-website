7bfaaf00247885e6f39a979e341e462068f1cff7
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2023-08-29 10:12:33 +0200

    chainlink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -970,7 +970,7 @@ export default function Example() {
                                 </span>
                                 <span data-subtitle="true" style={{ boxSizing: 'border-box', fontSize: '14px', lineHeight: '21px', letterSpacing: '-0.14px', color: 'rgb(102, 102, 102)', margin: '0px', maxWidth: '440.918px', outlineColor: 'rgb(37, 99, 235)', borderWidth: '0px', borderStyle: 'solid', borderColor: 'rgb(234, 234, 234)' }}>
                                     {/* Chainlink enhances smart contract capabilities by allowing access to data outside the blockchain and off-chain computing while keeping blockchain technology&apos;s security and reliability assurances. */}
-                                    Chainlink VRF provides cryptographically secure randomness that cannot be manipulated by any user, node operator, or malicious actor. It is an industry standard trusted by top projects.
+                                    Chainlink VRF provides cryptographically secure randomness that cannot be manipulated by any user, node operator, or malicious actor.
                                 </span>
                             </div>
                         </Link>
