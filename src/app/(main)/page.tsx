419e9cedf7b4a9903071a36c2c254ed2e03c2c7a
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2023-10-17 11:15:22 +0200

    Revert "remove launch draw"
    This reverts commit 91238a38fea0bd74272912ef640ff7aecd6f1b0e.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -324,7 +324,7 @@ export default function Example() {
                             </p>
                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                 <Link
-                                    href="#"
+                                    href="/launch-draw"
                                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
                                     Launch Draw
