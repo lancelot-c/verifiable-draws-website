fab7500ea9544b36c13688a0f36bd0cb6167df23
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2023-10-18 14:57:44 +0200

    new demo

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -329,7 +329,7 @@ export default function Example() {
                                 >
                                     Launch Draw
                                 </Link>
-                                <Link href="https://www.verify.win/bafkreifep7yhgryksbnonlgacbbw6ro2umwljk33epwn2zb26w3na76xkm" className="text-sm font-semibold leading-6 text-gray-900">
+                                <Link href="https://www.verify.win/bafkreicdqk6euz3frbgjg6xgam7ljmbzwsuqnm3bdztnkegk5igdourqkm" className="text-sm font-semibold leading-6 text-gray-900">
                                     View an example <span aria-hidden="true">→</span>
                                 </Link>
                             </div>
