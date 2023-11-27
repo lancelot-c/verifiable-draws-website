c361efa30d3efdfea551032b962541d5130a8642
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2023-11-27 19:08:19 +0100

    dec

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg> */}
-            Try Random.win for <span className="font-semibold">FREE</span> until December 25
+            Try Random.win for <span className="font-semibold">FREE</span> until Dec 25
             <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
